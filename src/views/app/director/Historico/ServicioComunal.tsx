176d9a4a72f42505e75ff80545ce8db3ee655c14
alberto munoz <albmunmu@gmail.com>
2024-07-11 23:26:22 -0600

    Added chips

--- src/views/app/director/Historico/ServicioComunal.tsx
+++ src/views/app/director/Historico/ServicioComunal.tsx
@@ -6,7 +6,7 @@ import { Row, Col, ModalBody, ModalHeader, Modal, Input as ReactstrapInput } fro
 import colors from '../../../../assets/js/colors'
 import withRouter from 'react-router-dom/withRouter'
 import Grid from '@material-ui/core/Grid'
-import { Checkbox, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@material-ui/core'
+import { Checkbox, Chip, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@material-ui/core'
 import StyledMultiSelect from '../../../../components/styles/StyledMultiSelect'
 import { ObtenerInfoCatalogos } from '../../../../redux/formularioCentroResponse/actions'
 import NavigationContainer from '../../../../components/NavigationContainer'
@@ -50,13 +50,14 @@ import "react-datepicker/dist/react-datepicker.css";
 export const ServicioComunal: React.FC<IProps> = props => {
 	const { t } = useTranslation()
 	const [catalogos, setCatalogos] = React.useState([])
-	const [areaProyecto, setData] = React.useState([])
+	const [areaProyecto, setAreaProyecto] = React.useState()
 	const [showAreaProyecto, setShowAreaProyecto] = React.useState(false)
 	const [showNombre, setShowNombre] = React.useState(false)
 	const [objetivoNombre, setObjetivoNombre] = React.useState([])
 	const [areasProyectoId, setAreasProyectoId] = React.useState()
 	const [currentExtentions, setCurrentExtentions] = React.useState()
 	const [nombresSeleccionados, setNombresSeleccionados] = React.useState([])
+	const [nombresIdSeleccionados, setNombresIdSeleccionados] = React.useState([])
 	const [showMap, setShowMap] = React.useState(false)
 	const [institutionImage, setInstitutionImage] = React.useState(null)
 	const [loading, setLoading] = React.useState<boolean>(false)
@@ -74,12 +75,11 @@ export const ServicioComunal: React.FC<IProps> = props => {
 	}, [])
 	return (
 		<AppLayout items={directorItems}>
-
 			{console.log('catalogos.areasProyecto', catalogos.areasProyecto)}
 			{loading && <BarLoader />}
 			{catalogos && console.log('catalogos', catalogos)}
 			{/* {showAreaProyecto && catalogos.areasProyecto &&
-				<ModalRadio title="Area Proyecto" setValue={setShowAreaProyecto} value={showAreaProyecto} coleccion={catalogos.areasProyecto} />} */}
+					<ModalRadio title="Area Proyecto" setValue={setShowAreaProyecto} value={showAreaProyecto} coleccion={catalogos.areasProyecto} />} */}
 			{showAreaProyecto && catalogos.areasProyecto &&
 				<SimpleModal title="Areas Proyecto" openDialog={showAreaProyecto} onConfirm={() => { setShowAreaProyecto(false) }} onClose={() => setShowAreaProyecto(false)}>
 					<FormControl>
@@ -89,33 +89,42 @@ export const ServicioComunal: React.FC<IProps> = props => {
 							name="radio-buttons-group"
 							value={value}
 						>
-							{catalogos.areasProyecto.map(item => <FormControlLabel value={item.id} onClick={(e, v) => { e.persist(); setValue(e.target.value) }} checked={value == item.id} control={<Radio />} label={item.nombre} />)}
+							{catalogos.areasProyecto.map(item => <FormControlLabel value={item.id} onClick={(e, v) => { e.persist(); console.log('item.nombre', item.nombre); setValue(e.target.value); setAreaProyecto(item.nombre) }} checked={value == item.id} control={<Radio />} label={item.nombre} />)}
 						</RadioGroup>
 					</FormControl>
 				</SimpleModal >}
-			{value}
-			{showNombre && catalogos.nombresProyecto &&
-				<SimpleModal title="Areas Proyecto" openDialog={showNombre} onConfirm={() => { setShowNombre(false) }} onClose={() => setShowNombre(false)}>
+			{console.log('nombresSeleccionados', nombresSeleccionados)}
+			{nombresSeleccionados && value && showNombre && catalogos.nombresProyecto &&
+				<SimpleModal title="Nmobre Proyecto" openDialog={showNombre} onConfirm={() => { setShowNombre(false) }} onClose={() => setShowNombre(false)}>
 					<FormControl>
 						<FormLabel id="demo-radio-buttons-group-label">Nombre Proyecto</FormLabel>
-						{console.log('nombresSeleccionados', nombresSeleccionados)}
-						{catalogos.nombresProyecto.filter(item => parseInt(item.codigo) == parseInt(value)).map(item =>
-							<FormControlLabel control={<Checkbox
-
-
-								checked={nombresSeleccionados.includes(item.id)}
-								onChange={(e, v) => {
-									console.log('ev,[e,v]', [e, v])
-									let nombres = nombresSeleccionados; if (nombres.includes(item.id)) { nombres = nombres.filter(x => x != item.id) } else { nombres.push(item.id) } setNombresSeleccionados(nombres);
+						{catalogos.nombresProyecto.filter(item => parseInt(item.codigo) == parseInt(value)).map((item, index) =>
+							<FormControlLabel
+								control={
+									<Checkbox
+										checked={nombresSeleccionados.includes(item.id)}
+										value={item.id}
+									/>
+								}
+								label={item.nombre}
+								onClick={async () => {
+									let nombres = [...nombresSeleccionados];
+									let nombresId = [...nombresIdSeleccionados];
+									console.log('nombres', nombres)
+									if (!nombresId.includes(item.id)) {
+										nombres.push(item)
+										nombresId.push(item.id)
+									} else {
+										nombres = nombres.filter(n => n != item.id)
+										nombresId = nombresId.filter(n => n.id != item.id)
+									}
+									setNombresSeleccionados(nombres);
+									setNombresIdSeleccionados(nombresId);
 								}}
-							/>}
-
-								label={item.nombre} />
+							/>
 						)}
-
-
 					</FormControl>
-				</SimpleModal >}
+				</SimpleModal>}
 			<NavigationContainer
 				goBack={() => {
 					props.history.push('/director/buscador/centro')
@@ -127,7 +136,7 @@ export const ServicioComunal: React.FC<IProps> = props => {
 					<Col sm={12}>
 						<Card className='bg-white__radius'>
 							<CardTitle>{t('registro_servicio_comunal', 'Registro Servicio Comunal')}</CardTitle>
-							
+							{nombresSeleccionados && console.log('nombresSeleccionados', nombresSeleccionados)}
 							<Form>
 								<Row>
 									<Col md={3}>
@@ -135,14 +144,19 @@ export const ServicioComunal: React.FC<IProps> = props => {
 											<Label>
 												{t('registro_servicio_comunal>area_proyecto', 'Area de proyecto')}
 											</Label>
-											<Input name='i' value={areaProyecto ? '' : areaProyecto} readOnly onClick={() => !showAreaProyecto && setShowAreaProyecto(true)} />
+											<Input key={areaProyecto} name='i' value={areaProyecto ? areaProyecto : ''} readOnly onClick={() => !showAreaProyecto && setShowAreaProyecto(true)} />
 										</FormGroup>
 									</Col>
 									<Col md={6}>
-										<FormGroup>
+										{nombresSeleccionados.length == 0 && <FormGroup>
 											<Label>{t('registro_servicio_comunal>objetivonombre', 'objetivo')}</Label>
 											<Input name='codigo' value={objetivoNombre ? '' : objetivoNombre} readOnly onClick={() => !showNombre && setShowNombre(true)} />
-										</FormGroup>
+										</FormGroup>}
+										{nombresSeleccionados.length > 0 &&
+											<div onClick={() => !showNombre && setShowNombre(true)}  style={{ padding: 8, background: '#e9ecef' }} > 
+												{nombresSeleccionados.map(n => <Chip label={n.nombre} />)}
+											</div>
+										}
 									</Col>
 									<Col sm={3}>
 										<FormGroup>
@@ -164,7 +178,7 @@ export const ServicioComunal: React.FC<IProps> = props => {
 										{' '}
 										<FormGroup>
 											<Label>
-										
+
 												{t('registro_servicio_comunal>fecha_conclusion', 'Fecha de conclusión SCE')}
 											</Label>
 											<DatePicker label="servicio comunal date picker" />
