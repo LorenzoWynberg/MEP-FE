05c8f0419db82eb3ee97a29774d6ae9c99c4c6af
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-08-11 20:25:55 -0600

    Adds minor fixes to reportes

--- src/views/app/reportes/Regional/ReporteUltimoNivelSinSCE/Reporte.tsx
+++ src/views/app/reportes/Regional/ReporteUltimoNivelSinSCE/Reporte.tsx
@@ -3,90 +3,64 @@ import styled from 'styled-components'
 import { TableReactImplementation } from 'Components/TableReactImplementation'
 
 const Reporte = ({ reportData, innerRef, loader = false }) => {
-  const tableMetadata = React.useMemo(() => {
-    const columns = [
-      {
-        Header: 'ID',
-        accessor: 'identificacion',
-        label: '',
-        column: ''
-      },
-      {
-        Header: 'Nombre',
-        accessor: 'nombreEstudiante',
-        label: '',
-        column: ''
-      },
-      {
-        Header: 'Nivel',
-        accessor: 'nombreNivel',
-        label: '',
-        column: ''
-      }
-    ]
-    /*
-		{
-			"nombreNivel": "AÑO 1",
-			"nombreGrupo": "BI 11-A",
-			"nombreEstudiante": "WILFRIDO JIMENEZ LEIVA",
-			"tipoIdentificacion": "Cédula de identidad costarricense",
-			"identificacion": "603100213",
-			"nombreInstitucion": "LICEO DE BAGACES",
-			"anioEducativo": "2022"
-		} */
-    const mapeador = (item) => {
-      return {
-        nivel: item?.nombreNivel,
-        grupo: item?.nombreGrupo,
-        persona: item?.nombreEstudiante,
-        tipoId: item?.tipoIdentificacion,
-        numeroId: item?.identificacion
-      }
-    }
-    const data = reportData ? reportData.map(mapeador) : []
-    /* for (let i = 0; i < 10; i++) {
-			data.push({
-				nivel:'PRIMER AÑO',
-				grupo:'1-2',
-				persona:'DANNY CHAVARRIA',
-				tipoId:'Cédula Nacional',
-				numeroId:'20.334.789',
-			})
-		} */
-    return {
-      columns,
-      data
-    }
-  }, [])
-  return (
-    <div ref={innerRef}>
-      <Card>
-        <h3>Resumen de cantidad de estudiantes de último nivel que no han concluido el Servicio Comunal Estudiantil</h3>
-        {/* <p>AÑO EDUCATIVO: 2022</p> */}
-        {/* <p>DIRECCION REGIONAL: ALAJUELA</p>
-				<p>CIRCUITO: 01</p>
-				<p>INSTITUCION: 6800 - BILINGUE LLAMA DEL BOSQUE</p> */}
-        {
-            !loader && (
-              <TableReactImplementation
-                avoidSearch
-                columns={tableMetadata.columns}
-                data={tableMetadata.data}
-              />
-            )
-          }
-      </Card>
-    </div>
-  )
+	const tableMetadata = React.useMemo(() => {
+		const columns = [
+			{
+				Header: 'ID',
+				accessor: 'identificacion',
+				label: '',
+				column: ''
+			},
+			{
+				Header: 'Nombre',
+				accessor: 'nombreEstudiante',
+				label: '',
+				column: ''
+			},
+			{
+				Header: 'Nivel',
+				accessor: 'nombreNivel',
+				label: '',
+				column: ''
+			}
+		]
+		const mapeador = item => {
+			return {
+				nivel: item?.nombreNivel,
+				grupo: item?.nombreGrupo,
+				persona: item?.nombreEstudiante,
+				tipoId: item?.tipoIdentificacion,
+				numeroId: item?.identificacion
+			}
+		}
+		const data = reportData ? reportData.map(mapeador) : []
+		return {
+			columns,
+			data
+		}
+	}, [])
+	return (
+		<div ref={innerRef}>
+			<Card>
+				<h3>
+					Resumen de cantidad de estudiantes de último nivel que no han concluido el Servicio
+					Comunal Estudiantil
+				</h3>
+				{!loader && (
+					<TableReactImplementation avoidSearch columns={tableMetadata.columns} data={tableMetadata.data} />
+				)}
+			</Card>
+		</div>
+	)
 }
 
 const Card = styled.div`
-  border-radius: 15px;
-  min-width: 100%;
-  min-height: 100%;
-  border-color: gray;
-  background: white;
-  padding: 15px;
+	border-radius: 15px;
+	min-width: 100%;
+	min-height: 100%;
+	border-color: gray;
+	background: white;
+	padding: 15px;
 `
 
 export default Reporte
--- src/views/app/reportes/Regional/ReporteUltimoNivelSinSCE/index.tsx
+++ src/views/app/reportes/Regional/ReporteUltimoNivelSinSCE/index.tsx
@@ -9,103 +9,102 @@ import { useSelector } from 'react-redux'
 import ReporteStyledTableCircuitos from '../../_partials/ReporteStyledTableCircuitos'
 
 const ReporteUltimoNivelSinSCE = ({ regresarEvent }) => {
-  const [state, setState] = React.useState(0)
-  const [idCircuitoSent, setIdCircuitoSent] = React.useState(0)
-  const [idRegionSent, setIdRegionSent] = React.useState(0)
-  const [reportData, setReportData] = React.useState<any>()
-  const [reportParameters, setReportParameters] = React.useState<any>()
-  const [loader, setLoader] = React.useState(false)
-  const printRef = React.useRef()
+	const [state, setState] = React.useState(0)
+	const [idCircuitoSent, setIdCircuitoSent] = React.useState(0)
+	const [idRegionSent, setIdRegionSent] = React.useState(0)
+	const [reportData, setReportData] = React.useState<any>()
+	const [reportParameters, setReportParameters] = React.useState<any>()
+	const [loader, setLoader] = React.useState(false)
+	const printRef = React.useRef()
 
-  const columns = [
-    {
-      Header: 'Codigo Institucion',
-      accessor: 'codigoInstitucion',
-      label: '',
-      column: ''
-    },
+	const columns = [
+		{
+			Header: 'Codigo Institucion',
+			accessor: 'codigoInstitucion',
+			label: '',
+			column: ''
+		},
+		{
+			Header: 'Institución',
+			accessor: 'nombreInstitucion',
+			label: '',
+			column: ''
+		},
+		{
+			Header: 'Hombres',
+			accessor: 'hombreCount',
+			label: '',
+			column: ''
+		},
+		{
+			Header: 'Mujeres',
+			accessor: 'mujerCount',
+			label: '',
+			column: ''
+		},
+		{
+			Header: 'Total Estudiantes',
+			accessor: 'totalEstudiantes',
+			label: '',
+			column: ''
+		}
+	]
 
-    {
-      Header: 'Hombres',
-      accessor: 'hombreCount',
-      label: '',
-      column: ''
-    },
+	const loadReportData = async regionId => {
+		try {
+			setLoader(true)
+			const response = await axios.get(
+				`${envVariables.BACKEND_URL}/api/ServicioComunal/Reportes/GetEstudiantesSinReqSCEUltNivel/${regionId}/0`
+			)
+			setLoader(false)
+			setReportData(response.data)
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-    {
-      Header: 'Mujeres',
-      accessor: 'mujerCount',
-      label: '',
-      column: ''
-    },
+	const onShowReportEvent = parametros => {
+		// setState(1)
+		const { circuitoId, regionId } = parametros
+		if (!regionId) return
 
-    {
-      Header: 'Intitución',
-      accessor: 'nombreInstitucion',
-      label: '',
-      column: ''
-    },
+		loadReportData(regionId.value).then(() => {
+			setReportParameters(parametros)
+			setIdRegionSent(regionId.value)
+			setState(1)
+		})
+	}
+	const onExcelEvent = () => {
+		const workbook = GenerateExcelObject(reportData)
+		SendWorkbookToDownload(workbook, `ReporteUltimoNivelSinSCE.xlsx`)
+	}
 
-    {
-      Header: 'Total Estudiantes',
-      accessor: 'totalEstudiantes',
-      label: '',
-      column: ''
-    },
-
-  ]
-
-  const loadReportData = async ( regionId) => {
-    try {
-      setLoader(true)
-      const response = await axios.get(
-        `${envVariables.BACKEND_URL}/api/ServicioComunal/Reportes/GetEstudiantesSinReqSCEUltNivel/${regionId}/0`
-      )
-      setLoader(false)
-      setReportData(response.data)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  const onShowReportEvent = (parametros) => {
-    // setState(1)
-    const { circuitoId, regionId } = parametros
-    if (  !regionId) return
-
-    loadReportData(  regionId.value).then(() => {
-      setReportParameters(parametros)
-      setIdRegionSent(regionId.value)
-      setState(1)
-    })
-  }
-  const onExcelEvent = () => {
-    const workbook = GenerateExcelObject(reportData)
-    SendWorkbookToDownload(workbook, `ReporteUltimoNivelSinSCE.xlsx`)
-  }
-
-  return (
-    <div> 
-      <ReportBar
-        regresarEvent={() => {
-          regresarEvent()
-          setState(0)
-        }}
-        onExcelBtnEvent={onExcelEvent}
-        imprimirRef={printRef}
-        showBtn={state === 1}
-      />
-      {state === 0 && (
-        <Parameters
-          showReportEvent={onShowReportEvent}
-          reportLoader={loader}
-        />
-      )}
-      {state === 1 && (
-        <ReporteStyledTableCircuitos innerRef={printRef} data={reportData} idCircuito={null} idRegion={idRegionSent} columns={columns} title={'Resumen de cantidad de estudiantes de último nivel que no han concluido el Servicio Comunal Estudiantil'}
-        />)}
-    </div>
-  )
+	return (
+		<div>
+			<ReportBar
+				regresarEvent={() => {
+					regresarEvent()
+					setState(0)
+				}}
+				onExcelBtnEvent={onExcelEvent}
+				imprimirRef={printRef}
+				showBtn={state === 1}
+			/>
+			{state === 0 && <Parameters showReportEvent={onShowReportEvent} reportLoader={loader} />}
+			{state === 1 && (
+				<ReporteStyledTableCircuitos
+					innerRef={printRef}
+					data={reportData}
+					idCircuito={null}
+					idRegion={idRegionSent}
+					columns={columns}
+					title={
+						'Resumen de cantidad de estudiantes de último nivel que no han concluido el Servicio Comunal Estudiantil'
+					}
+				/>
+			)}
+		</div>
+	)
 }
 
 export default ReporteUltimoNivelSinSCE
