94139e4261f713bfb11dd405e0d473f11e58744a
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-09-11 11:31:23 -0600

    Removes email from validation fields

--- src/views/app/director/ExpedienteEstudiante/_partials/contacto/InformacionContacto.js
+++ src/views/app/director/ExpedienteEstudiante/_partials/contacto/InformacionContacto.js
@@ -26,161 +26,157 @@ import { useTranslation } from 'react-i18next'
 import axios from 'axios'
 import { envVariables } from '../../../../../../constants/enviroment'
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  control: {
-    padding: theme.spacing(2)
-  },
-  paper: {
-    minHeight: 475,
-    padding: 20,
-    marginLeft: 10,
-    borderRadius: 15,
-    boxShadow: '2px 2px 5px rgba(228, 226, 226, 0.856)'
-  }
+	root: {
+		flexGrow: 1
+	},
+	control: {
+		padding: theme.spacing(2)
+	},
+	paper: {
+		minHeight: 475,
+		padding: 20,
+		marginLeft: 10,
+		borderRadius: 15,
+		boxShadow: '2px 2px 5px rgba(228, 226, 226, 0.856)'
+	}
 }))
 
 const InformacionContacto = props => {
 	const { t } = useTranslation()
 	console.log('props.informacionContacto', props.expedienteEstudiantil.currentStudent)
 
-  const classes = useStyles()
-  const [snakbar, handleClick, handleClose] = useNotification()
-  const [snackbarMsg, setSnackbarMsg] = useState('')
-  const [snackbarVariant, setSnackbarVariant] = useState('success')
-  const [editable, setEditable] = useState(false)
-  const [loading, setLoading] = useState(true)
-  const [errorFields, setErrorFields] = useState(props.informacionContacto.errorFields)
-  const [errorMessages, setErrorMessages] = useState(props.informacionContacto.errorMessages)
+	const classes = useStyles()
+	const [snakbar, handleClick, handleClose] = useNotification()
+	const [snackbarMsg, setSnackbarMsg] = useState('')
+	const [snackbarVariant, setSnackbarVariant] = useState('success')
+	const [editable, setEditable] = useState(false)
+	const [loading, setLoading] = useState(true)
+	const [errorFields, setErrorFields] = useState(props.informacionContacto.errorFields)
+	const [errorMessages, setErrorMessages] = useState(props.informacionContacto.errorMessages)
 
+	useEffect(() => {
+		const fetchData = async () => {
+			await props.getInformationContactFromUser(props.expedienteEstudiantil.currentStudent.idEstudiante)
+			setLoading(false)
+		}
+		fetchData()
+		return () => {
+			props.cleanInformationContactFromUser()
+		}
+	}, [])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await props.getInformationContactFromUser(props.expedienteEstudiantil.currentStudent.idEstudiante)
-      setLoading(false)
-    }
-    fetchData()
-    return () => {
-      props.cleanInformationContactFromUser()
-    }
-  }, [])
+	const [formState, setFormState] = useState({
+		telefono: '',
+		telefonoSecundario: '',
+		emailSecundario: '',
+		facebook: '',
+		twitter: '',
+		instagram: '',
+		whatsapp: ''
+	})
+	useEffect(() => {
+		axios
+			.get(
+				`${envVariables.BACKEND_URL}/api/ExpedienteEstudiante/Contacto/${props.expedienteEstudiantil.currentStudent.idEstudiante}`
+			)
+			.then(r => {
+				setFormState({ ...props.informacionContacto.contactInformation, ...r.data })
+				setErrorFields(props.informacionContacto.errorFields)
+				setErrorMessages(props.informacionContacto.errorMessages)
+			})
+	}, [props.informacionContacto, editable])
 
-  const [formState, setFormState] = useState({
-    telefono: '',
-    telefonoSecundario: '',
-    email: '',
-    emailSecundario: '',
-    facebook: '',
-    twitter: '',
-    instagram: '',
-    whatsapp: ''
-  })
-  useEffect(() => {
-    axios
-      .get(
-        `${envVariables.BACKEND_URL}/api/ExpedienteEstudiante/Contacto/${props.expedienteEstudiantil.currentStudent.idEstudiante}`
-      )
-      .then(r => {
+	const onSubmit = useCallback(
+		async e => {
+			e.preventDefault()
+			console.log('formState', formState)
+			props
+				.updateInformationContactFromUser(props.expedienteEstudiantil.currentStudent.idEstudiante, formState)
+				.then(rest => {
+					setSnackbarVariant('success')
+					setSnackbarMsg(t('general>success_act', 'Se actualizo correctamente'))
+					handleClick()
+					setEditable(false)
+				})
+				.catch(err => {
+					setSnackbarVariant('error')
+					setSnackbarMsg(t('general>error_act', 'Ocurrio un error al actualizar'))
+					handleClick()
+				})
+		},
+		[formState]
+	)
 
-        setFormState({ ...props.informacionContacto.contactInformation, ...r.data, })
-        setErrorFields(props.informacionContacto.errorFields)
-        setErrorMessages(props.informacionContacto.errorMessages)
-      })
-  }, [props.informacionContacto, editable])
+	const handleInputChange = ({ target }) => {
+		if (editable) {
+			setFormState({ ...formState, [target.name]: target.value })
+		}
+	}
 
-
-  const onSubmit = useCallback(
-    async e => {
-      e.preventDefault()
-      console.log('formState', formState)
-      props.updateInformationContactFromUser(
-        props.expedienteEstudiantil.currentStudent.idEstudiante,
-        formState
-      ).then(rest => {
-        setSnackbarVariant('success')
-        setSnackbarMsg(t('general>success_act', 'Se actualizo correctamente'))
-        handleClick()
-        setEditable(false)
-      }).catch(err => {
-        setSnackbarVariant('error')
-        setSnackbarMsg(t('general>error_act', 'Ocurrio un error al actualizar'))
-        handleClick()
-      })
-
-
-    }, [formState])
-
-  const handleInputChange = ({ target }) => {
-    if (editable) {
-      setFormState({ ...formState, [target.name]: target.value })
-    }
-  }
-
-  return (
-    <Grid container className={classes.root} spacing={2}>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          {snakbar(snackbarVariant, snackbarMsg)}
-          <Grid item md={6} xs={12}>
-            <Paper className={classes.paper}>
-              <Grid container>
-                <Grid item xs={12} className={classes.control}>
-                  <h4>
-                    {t('expediente_ce>informacion_general>informacion', 'Información de contacto')}
-                  </h4>
-                </Grid>
-                <Grid item xs={12} className={classes.control}>
-                  <FormGroup>
-                    <Label>
-                      *{' '}
-                      {t(
-                        'estudiantes>expediente>contacto>info_cont>tel_prin',
-                        'Teléfono principal'
-                      )}
-                    </Label>
-                    <ReactInputMask
-                      mask='9999-9999'
-                      type='text'
-                      name='telefono'
-                      id='telefono'
-                      placeholder='8888-8888'
-                      value={formState.telefono}
-                      disabled={!editable}
-                      onChange={handleInputChange}
-                      invalid={errorFields.Telefono}
-                    >
-                      {inputProps => <Input {...inputProps} disabled={!editable} />}
-                    </ReactInputMask>
-                    <FormFeedback>{errorMessages.Telefono}</FormFeedback>
-                  </FormGroup>
-
-                  <FormGroup>
-                    <Label>
-                      {t(
-                        'estudiantes>expediente>contacto>info_cont>tel_alter',
-                        'Teléfono alternativo'
-                      )}
-                    </Label>
-                    <ReactInputMask
-                      mask='9999-9999'
-                      type='text'
-                      name='telefonoSecundario'
-                      id='telefonoSecundario'
-                      placeholder='8888-8888'
-                      value={formState.telefonoSecundario}
-                      disabled={!editable}
-                      onChange={handleInputChange}
-                      invalid={errorFields.TelefonoSecundario}
-                    >
-                      {inputProps => <Input {...inputProps} disabled={!editable} />}
-                    </ReactInputMask>
-                    <FormFeedback>{errorMessages.TelefonoSecundario}</FormFeedback>
-                  </FormGroup>
+	return (
+		<Grid container className={classes.root} spacing={2}>
+			{loading ? (
+				<Loader />
+			) : (
+				<>
+					{snakbar(snackbarVariant, snackbarMsg)}
+					<Grid item md={6} xs={12}>
+						<Paper className={classes.paper}>
+							<Grid container>
+								<Grid item xs={12} className={classes.control}>
+									<h4>
+										{t('expediente_ce>informacion_general>informacion', 'Información de contacto')}
+									</h4>
+								</Grid>
+								<Grid item xs={12} className={classes.control}>
 									<FormGroup>
 										<Label>
+											*{' '}
+											{t(
+												'estudiantes>expediente>contacto>info_cont>tel_prin',
+												'Teléfono principal'
+											)}
+										</Label>
+										<ReactInputMask
+											mask='9999-9999'
+											type='text'
+											name='telefono'
+											id='telefono'
+											placeholder='8888-8888'
+											value={formState.telefono}
+											disabled={!editable}
+											onChange={handleInputChange}
+											invalid={errorFields.Telefono}
+										>
+											{inputProps => <Input {...inputProps} disabled={!editable} />}
+										</ReactInputMask>
+										<FormFeedback>{errorMessages.Telefono}</FormFeedback>
+									</FormGroup>
+
+									<FormGroup>
+										<Label>
+											{t(
+												'estudiantes>expediente>contacto>info_cont>tel_alter',
+												'Teléfono alternativo'
+											)}
+										</Label>
+										<ReactInputMask
+											mask='9999-9999'
+											type='text'
+											name='telefonoSecundario'
+											id='telefonoSecundario'
+											placeholder='8888-8888'
+											value={formState.telefonoSecundario}
+											disabled={!editable}
+											onChange={handleInputChange}
+											invalid={errorFields.TelefonoSecundario}
+										>
+											{inputProps => <Input {...inputProps} disabled={!editable} />}
+										</ReactInputMask>
+										<FormFeedback>{errorMessages.TelefonoSecundario}</FormFeedback>
+									</FormGroup>
+									<FormGroup>
+										<Label for='emailSecundario'>
 											*
 											{t(
 												'estudiantes>expediente>contacto>info_cont>correo_per',
@@ -200,7 +196,7 @@ const InformacionContacto = props => {
 										<FormFeedback>{errorMessages.Email}</FormFeedback>
 									</FormGroup>
 									<FormGroup>
-										<Label for='emailSecundario'>
+										<Label for='email'>
 											{t(
 												'estudiantes>expediente>contacto>info_cont>correo_inst',
 												'Correo electrónico institucional'
@@ -214,95 +210,92 @@ const InformacionContacto = props => {
 											onChange={handleInputChange}
 											disabled
 											value={formState.email}
-											invalid={errorFields.Email}
 										/>
-										<FormFeedback>{errorMessages.EmailSecundario}</FormFeedback>
 									</FormGroup>
 								</Grid>
 							</Grid>
 						</Paper>
 					</Grid>
 
-          <Grid item md={6} xs={12}>
-            <Paper className={classes.paper}>
-              <Grid container>
-                <Grid item xs={12} className={classes.control}>
-                  <h4>
-                    {t('expediente_ce>informacion_general>informacion>redes', 'Redes sociales')}
-                  </h4>
-                </Grid>
-                <Grid item xs={12} className={classes.control}>
-                  <Redes
-                    hasEditable={editable}
-                    handleInputChange={handleInputChange}
-                    formState={formState}
-                  />
+					<Grid item md={6} xs={12}>
+						<Paper className={classes.paper}>
+							<Grid container>
+								<Grid item xs={12} className={classes.control}>
+									<h4>
+										{t('expediente_ce>informacion_general>informacion>redes', 'Redes sociales')}
+									</h4>
+								</Grid>
+								<Grid item xs={12} className={classes.control}>
+									<Redes
+										hasEditable={editable}
+										handleInputChange={handleInputChange}
+										formState={formState}
+									/>
+								</Grid>
+							</Grid>
+						</Paper>
+					</Grid>
 
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-
-          <Grid item xs={12} style={{ textAlign: 'center' }} className={classes.control}>
-            <Grid item xs={12} style={{ textAlign: 'center' }} className={classes.control}>
-              {props.informacionContacto.contactInformation.loading ? (
-                <>
-                  <Loader formLoader />
-                </>
-              ) : (
-                <>
-                  {editable ? (
-                    <>
-                      <Button
-                        color='secundary'
-                        className='btn-shadow m-0'
-                        type='button'
-                        onClick={() => {
-                          setEditable(false)
-                          props.cleanFormErrors()
-                        }}
-                      >
-                        {t('general>cancelar', 'Cancelar')}
-                      </Button>
-                      <Button
-                        color='primary'
-                        className='btn-shadow m-0'
-                        type='button'
-                        onClick={onSubmit}
-                      >
-                        {t('general>guardar', 'Guardar')}
-                      </Button>
-                    </>
-                  ) : (
-                    <Button
-                      color='primary'
-                      className='btn-shadow m-0'
-                      type='button'
-                      onClick={() => {
-                        setEditable(true)
-                      }}
-                    >
-                      {t('general>editar', 'Editar')}
-                    </Button>
-                  )}
-                </>
-              )}
-            </Grid>
-          </Grid>
-        </>
-      )}
-    </Grid>
-  )
+					<Grid item xs={12} style={{ textAlign: 'center' }} className={classes.control}>
+						<Grid item xs={12} style={{ textAlign: 'center' }} className={classes.control}>
+							{props.informacionContacto.contactInformation.loading ? (
+								<>
+									<Loader formLoader />
+								</>
+							) : (
+								<>
+									{editable ? (
+										<>
+											<Button
+												color='secundary'
+												className='btn-shadow m-0'
+												type='button'
+												onClick={() => {
+													setEditable(false)
+													props.cleanFormErrors()
+												}}
+											>
+												{t('general>cancelar', 'Cancelar')}
+											</Button>
+											<Button
+												color='primary'
+												className='btn-shadow m-0'
+												type='button'
+												onClick={onSubmit}
+											>
+												{t('general>guardar', 'Guardar')}
+											</Button>
+										</>
+									) : (
+										<Button
+											color='primary'
+											className='btn-shadow m-0'
+											type='button'
+											onClick={() => {
+												setEditable(true)
+											}}
+										>
+											{t('general>editar', 'Editar')}
+										</Button>
+									)}
+								</>
+							)}
+						</Grid>
+					</Grid>
+				</>
+			)}
+		</Grid>
+	)
 }
 
 const mapStateToProps = state => ({
-  informacionContacto: state.informacionContacto,
-  expedienteEstudiantil: state.expedienteEstudiantil
+	informacionContacto: state.informacionContacto,
+	expedienteEstudiantil: state.expedienteEstudiantil
 })
 
 export default withAuthorization({
-  id: 2,
-  Modulo: 'Expediente Estudiantil',
-  Apartado: 'Informacion de Contacto',
-  Seccion: 'Informacion de Contacto'
+	id: 2,
+	Modulo: 'Expediente Estudiantil',
+	Apartado: 'Informacion de Contacto',
+	Seccion: 'Informacion de Contacto'
 })(connect(mapStateToProps, contactActions)(withRouter(InformacionContacto)))
