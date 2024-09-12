723a17ff1e006fab76341135ca24fa523eed6638
alberto munoz <albmunmu@gmail.com>
2024-09-11 22:17:51 -0600

    Login White Page fix

--- src/containers/navs/Topnav.js
+++ src/containers/navs/Topnav.js
@@ -358,6 +358,7 @@ class TopNav extends Component {
       activeYears,
       selectedActiveYear
     } = this.props
+    console.log('authObject',authObject)
     const token = authObject.user.token
     const { data } = this.state
     const isEncargadoOrEstudiante =
@@ -370,7 +371,7 @@ class TopNav extends Component {
     const institutionsArray = isEncargadoOrEstudiante
       ? ''
       : authObject.user.rolesOrganizaciones.map((el) => {
-        const institution = authObject.user.instituciones.find(
+        const institution = authObject.user.instituciones?.find(
           (item) => item.id === parseInt(el.organizacionId)
         )
         return { ...el, institutionObject: institution }
