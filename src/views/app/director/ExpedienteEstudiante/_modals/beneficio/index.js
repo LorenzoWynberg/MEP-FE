cba8dfa549cf5fec3ca9f1e8afe3041db867c8fb
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-09-12 09:53:21 -0600

    Clean up

--- src/views/app/director/ExpedienteEstudiante/_modals/beneficio/index.js
+++ src/views/app/director/ExpedienteEstudiante/_modals/beneficio/index.js
@@ -1,52 +1,56 @@
 import React from 'react'
-import { Modal, ModalHeader, ModalBody, Button, Row, Col, Container, Input, Label } from 'reactstrap'
+import { Row, Col, Container, Input, Label } from 'reactstrap'
+import OptionModal from 'Components/Modal/OptionModal'
 
-import styled from 'styled-components'
-import OptionModal from '../../../../../../components/Modal/OptionModal'
+const Subsidio = props => {
+	const { open, titulo, toggleModal } = props
 
-const Subsidio = (props) => {
-  const { open, titulo, toggleModal } = props
+	return (
+		<OptionModal
+			isOpen={open}
+			titleHeader={titulo || 'Tipo de subsidio MEP'}
+			onCancel={toggleModal}
+			onConfirm={() => toggleModal(true)}
+		>
+			<Container className="modal-detalle-subsidio">
+				<Row>
+					<Col xs={12}>
+						{props.tipos.map(item => {
+							return (
+								<Row key={item.id}>
+									<Col xs={3} className="modal-detalle-subsidio-col">
+										<div>
+											<Label className="cursor-pointer">
+												<Input
+													type="radio"
+													inline
+													onClick={() => props.handleChangeSubsidio(item)}
+													checked={props.currentSubsidio.id === item.id}
+												/>
 
-  return (
-    <OptionModal isOpen={open} titleHeader={titulo || 'Tipo de subsidio MEP'} onCancel={toggleModal} onConfirm={() => toggleModal(true)}>
-      <Container className='modal-detalle-subsidio'>
-        <Row>
-          <Col xs={12}>
-            {props.tipos.map((item) => {
-              return (
-                <Row>
-                  <Col xs={3} className='modal-detalle-subsidio-col'>
-                    <div>
-                      <Label className='cursor-pointer'>
-                        <Input
-                          type='radio'
-                          inline
-                          onClick={() => props.handleChangeSubsidio(item)}
-                          checked={props.currentSubsidio.id === item.id}
-                        />
-
-                        <p>{item.nombre}</p>
-                      </Label>
-                    </div>
-                  </Col>
-                  <Col xs={9} className='modal-detalle-subsidio-col'>
-                    <div>
-                      <p>
-                        {item.detalle
-                          ? item.detalle
-                          : item.descripcion ? item.descripcion : 'Elemento sin detalle actualmente'}
-                      </p>
-                    </div>
-                  </Col>
-                </Row>
-              )
-            })}
-          </Col>
-        </Row>
-      </Container>
-    </OptionModal>
-  )
+												<p>{item.nombre}</p>
+											</Label>
+										</div>
+									</Col>
+									<Col xs={9} className="modal-detalle-subsidio-col">
+										<div>
+											<p>
+												{item.detalle
+													? item.detalle
+													: item.descripcion
+													? item.descripcion
+													: 'Elemento sin detalle actualmente'}
+											</p>
+										</div>
+									</Col>
+								</Row>
+							)
+						})}
+					</Col>
+				</Row>
+			</Container>
+		</OptionModal>
+	)
 }
-
 
 export default Subsidio
