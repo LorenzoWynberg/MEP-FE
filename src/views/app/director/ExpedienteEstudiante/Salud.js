3e4ad21d271e5b9e48fa889d02fa28f3a173fe53
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-09-20 10:52:17 -0600

    Permisos salud

--- src/views/app/director/ExpedienteEstudiante/Salud.js
+++ src/views/app/director/ExpedienteEstudiante/Salud.js
@@ -1,18 +1,23 @@
-import React, { useEffect, useState, useMemo } from 'react'
-import { useSelector } from 'react-redux'
-import SaludForm from './_partials/salud/SaludForm'
-import HTMLTable from '../../../../components/HTMLTable'
-import IntlMessages from '../../../../helpers/IntlMessages'
-import SaludChart from './_partials/salud/SaludChart'
-import { Button } from 'reactstrap'
-import styled from 'styled-components'
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
-import { connect } from 'react-redux'
+import { TableReactImplementation } from 'Components/TableReactImplementation'
+import withAuthorization from '../../../../Hoc/withAuthorization'
 import useNotification from '../../../../hooks/useNotification'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import IntlMessages from '../../../../helpers/IntlMessages'
+import React, { useEffect, useState, useMemo } from 'react'
+import { isEmptyNullOrUndefined } from 'Utils/validators'
 import CofirmModal from 'Components/common/ConfirmModal'
+import BarLoader from 'Components/barLoader/barLoader'
+import SaludChart from './_partials/salud/SaludChart'
+import SaludForm from './_partials/salud/SaludForm'
+import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Edit'
-import colors from 'Assets/js/colors'
+import { useTranslation } from 'react-i18next'
 import Tooltip from '@mui/material/Tooltip'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+import { connect } from 'react-redux'
+import colors from 'Assets/js/colors'
+import { clamp } from 'lodash'
 
 import {
 	addSalud,
@@ -23,11 +28,6 @@ import {
 	cleanCurrentSaludItem
 } from 'Redux/salud/actions'
 
-import { isEmptyNullOrUndefined } from 'Utils/validators'
-import withAuthorization from '../../../../Hoc/withAuthorization'
-import { TableReactImplementation } from 'Components/TableReactImplementation'
-import { useTranslation } from 'react-i18next'
-
 const Salud = props => {
 	const { t } = useTranslation()
 	const state = useSelector(store => {
@@ -36,7 +36,9 @@ const Salud = props => {
 		}
 	})
 	const identificacion = state.identification.data.identificacion
-	const nacionalItem = state.identification.data.datos.find(item => item.catalogoId === 1 && item.elementoId === 1)
+	const nacionalItem = state.identification.data.datos.find(
+		item => item.catalogoId === 1 && item.elementoId === 1
+	)
 	const nacional = nacionalItem !== undefined
 
 	const {
@@ -50,26 +52,24 @@ const Salud = props => {
 		cleanCurrentSaludItem
 	} = props
 
+	const [loading, setLoading] = useState(false)
 	const [addEdit, setAddEdit] = useState(false)
-	const [editable, setEditable] = useState(false)
 	const [saludItem, setSaludItem] = useState({})
+	const [editable, setEditable] = useState(false)
 	const [openConFirm, setOpenConFirm] = useState(false)
 	const [itemToDelete, setItemToDelete] = useState({})
-
-	const [snackbarContent, setSnacbarContent] = useState({
+	const [snackbarContent, setSnackbarContent] = useState({
 		msg: 'welcome',
 		variant: 'info'
 	})
 	const [snackBar, handleClick] = useNotification()
 
 	useEffect(() => {
-		console.log('initialProps', state)
 		const fetchItems = async () => {
-			const response = await getSaludItems(identidadId)
+			await getSaludItems(identidadId)
 		}
-
 		fetchItems()
-	}, [getSaludItems, identidadId])
+	}, [identidadId])
 
 	useEffect(() => {
 		setSaludItem(salud.currentItem)
@@ -84,19 +84,28 @@ const Salud = props => {
 			{
 				column: 'seguroSocial',
 				label: 'Número de seguro social',
-				Header: t('estudiantes>expediente>salud>col_num_seguro', 'Número de seguro social'),
+				Header: t(
+					'estudiantes>expediente>salud>col_num_seguro',
+					'Número de seguro social'
+				),
 				accessor: 'seguroSocial'
 			},
 			{
 				column: 'date',
 				label: 'Fecha del registro del sistema',
-				Header: t('estudiantes>expediente>salud>col_fecha_registro', 'Fecha del registro del sistema'),
+				Header: t(
+					'estudiantes>expediente>salud>col_fecha_registro',
+					'Fecha del registro del sistema'
+				),
 				accessor: 'date'
 			},
 			{
 				column: 'usuario',
 				label: 'Usuario que registró el dato',
-				Header: t('estudiantes>expediente>salud>col_usuario_registro', 'Usuario que registró el dato'),
+				Header: t(
+					'estudiantes>expediente>salud>col_usuario_registro',
+					'Usuario que registró el dato'
+				),
 				accessor: 'usuario'
 			},
 			{
@@ -118,7 +127,10 @@ const Salud = props => {
 			{
 				column: 'imc',
 				label: 'Índice de masa corporal (imc)',
-				Header: t('estudiantes>expediente>salud>imc', 'Índice de masa corporal (imc)'),
+				Header: t(
+					'estudiantes>expediente>salud>imc',
+					'Índice de masa corporal (imc)'
+				),
 				accessor: 'imc'
 			},
 			{
@@ -126,82 +138,105 @@ const Salud = props => {
 				label: 'Acciones',
 				Header: t('general>acciones', 'Acciones'),
 				accessor: 'actions',
-				Cell: ({ row }) => (
-					<div className='d-flex justify-content-center align-items-center' style={{ cursor: 'pointer' }}>
-						<Tooltip title={t('general>editar', 'Editar')}>
-							<EditIcon
-								style={{
-									color: colors.darkGray
-								}}
-								onClick={() => {
-									setAddEdit(true)
-									setCurrentItem(row.original.id)
-									setEditable(true)
-								}}
-							/>
-						</Tooltip>
-					</div>
-				)
+				Cell: ({ row }) => showActions(row)
 			}
 		],
 		[t]
 	)
 
-	const handleDelete = data => {
-		setOpenConFirm(true)
-		setItemToDelete(data)
+	const showActions = row => {
+		return (
+			<div
+				className="d-flex justify-content-center align-items-center"
+				style={{ cursor: 'pointer' }}
+			>
+				<Tooltip title={t('general>editar', 'Editar')}>
+					<EditIcon
+						style={
+							props.validations.modificar
+								? { color: colors.darkGray }
+								: { display: 'none' }
+						}
+						onClick={() => {
+							setAddEdit(true)
+							setCurrentItem(row.original.id)
+							setEditable(true)
+						}}
+					/>
+				</Tooltip>
+				<Tooltip title={t('general>eliminar', 'Eliminar')}>
+					<DeleteIcon
+						style={
+							props.validations.eliminar
+								? { color: colors.darkGray }
+								: { display: 'none' }
+						}
+						onClick={() => handleDelete(row.original)}
+					/>
+				</Tooltip>
+			</div>
+		)
 	}
+
+	const handleDelete = data => {
+		setItemToDelete(data)
+		setOpenConFirm(true)
+	}
+
 	const onCloseDialogDelete = () => {
 		setOpenConFirm(false)
 		setItemToDelete({})
 	}
+
 	const onConfirmDialogDelete = () => {
+		setLoading(true)
 		setOpenConFirm(false)
 		setTimeout(() => {
 			onSelectRowDelete()
 		}, 500)
 	}
+
 	const onSelectRowDelete = async () => {
 		const response = await deleteSalud(itemToDelete.id)
-
 		if (response.error) {
-			setSnacbarContent({
+			setSnackbarContent({
 				variant: 'error',
-				msg: <IntlMessages id='error.delete' />
+				msg: <IntlMessages id="error.delete" />
 			})
 		} else {
-			setSnacbarContent({
+			setSnackbarContent({
 				variant: 'info',
-				msg: <IntlMessages id='done.delete' />
+				msg: <IntlMessages id="done.delete" />
 			})
 		}
+		setLoading(false)
 		handleClick()
 	}
-	const handleEdit = data => {
+
+	const handleModal = (data, edit = false) => {
 		setAddEdit(true)
-		setCurrentItem(data.id)
+		setCurrentItem(edit ? data.id : {})
 		setEditable(true)
 	}
 
-	const actionRow = [
-		{
-			actionName: 'button.edit',
-			actionFunction: handleEdit,
-			actionDisplay: data => {
-				return data.showActionRow
-			}
-		}
-	]
-
 	const getIMC = (peso, talla) => {
-		peso = isEmptyNullOrUndefined(peso) ? 0 : peso
-		talla = isEmptyNullOrUndefined(talla) ? 0 : talla
-		return [peso, talla].includes(0) ? '' : parseFloat(peso / Math.pow(talla / 100, 2)).toFixed(2)
+		if (isNaN(peso) || isNaN(talla) || peso === 0 || talla === 0) return ''
+		return (peso / Math.pow(talla / 100, 2)).toFixed(2)
 	}
 
 	const handleChange = e => {
 		const { name, value } = e.target
-		const saludItemChanged = { ...saludItem, [name]: value }
+		let newValue = value
+
+		if (name === 'peso') {
+			const parsedValue = parseInt(value, 10)
+			newValue = isNaN(parsedValue) ? '' : clamp(parsedValue, 1, 250)
+		} else if (name === 'talla') {
+			const parsedValue = parseInt(value, 10)
+			newValue = isNaN(parsedValue) ? '' : clamp(parsedValue, 1, 250)
+		}
+
+		const saludItemChanged = { ...saludItem, [name]: newValue }
 
 		setSaludItem({
 			...saludItemChanged,
@@ -219,12 +254,6 @@ const Salud = props => {
 			talla: isEmptyNullOrUndefined(saludItem.talla) ? 0 : saludItem.talla,
 			imc: isEmptyNullOrUndefined(saludItem.imc) ? 0 : saludItem.imc
 		}
-		// if (nacional) {
-		// 	_data = {
-		// 		..._data,
-		// 		seguroSocial: identificacion
-		// 	}
-		// }
 
 		let response
 		if (!_data.id) {
@@ -234,13 +263,13 @@ const Salud = props => {
 		}
 
 		if (response.error) {
-			setSnacbarContent({
+			setSnackbarContent({
 				variant: 'error',
 				msg: response.error
 			})
 		} else {
-			const msgText = !_data.id ? 'agregado' : 'actualizado'
-			setSnacbarContent({
+			const msgText = !_data.id ? 'agregados' : 'actualizados'
+			setSnackbarContent({
 				variant: 'success',
 				msg: `Datos ${msgText} con exito`
 			})
@@ -252,61 +281,45 @@ const Salud = props => {
 	}
 
 	const showSnackbar = (variant, msg) => {
-		setSnacbarContent({
+		setSnackbarContent({
 			variant,
 			msg
 		})
 	}
+
+	if (loading) {
+		return <BarLoader />
+	}
+
 	return (
 		<>
 			{!addEdit ? (
 				<>
 					{snackBar(snackbarContent.variant, snackbarContent.msg)}
-					<h4>{t('estudiantes>expediente>salud>titulo', 'Información de salud')}</h4>
+					<h4>
+						{t('estudiantes>expediente>salud>titulo', 'Información de salud')}
+					</h4>
 					<br />
 					{salud.items.length > 0 ? <SaludChart items={salud.items} /> : null}
 
-					<h4>{t('estudiantes>expediente>salud>valoracion_nutri', 'Valoración nutricional')}</h4>
+					<h4>
+						{t(
+							'estudiantes>expediente>salud>valoracion_nutri',
+							'Valoración nutricional'
+						)}
+					</h4>
 
 					<br />
-					<div className='mb-5'>
+					<div className="mb-5">
 						<TableReactImplementation
-							showAddButton 
-							onSubmitAddButton={() => {
-								props.authHandler(
-									'agregar',
-									() => {
-										setAddEdit(true)
-										setCurrentItem()
-										setEditable(true)
-									},
-									showSnackbar
-								)
-							}}
-							msjButton='Agregar'
+							showAddButton={props.validations.agregar}
+							onSubmitAddButton={handleModal}
+							msjButton="Agregar"
 							columns={columns}
-							data={salud.items} 
+							data={salud.items}
 							avoidSearch
 						/>
 					</div>
-					{/* <HTMLTable
-            columns={columns}
-            selectDisplayMode="datalist"
-            data={salud.items}
-            tableName="label.salud"
-            showHeaders={true}
-            loading={salud.loading}
-            actionRow={actionRow}
-            showMenu={(data) => {
-              return data.showActionRow;
-            }}
-            PageHeading={false}
-            readOnly
-            disableSearch
-            toggleEditModal={() => null}
-            hideMultipleOptions={true}
-            showHeadersCenter={false}
-          /> */}
 					{openConFirm && (
 						<CofirmModal
 							openDialog={openConFirm}
@@ -327,11 +340,19 @@ const Salud = props => {
 						<h4>{t('edit_button>regresar', 'REGRESAR')}</h4>
 					</NavigationContainer>
 					<SaludForm
-						setEditable={value => props.authHandler('modificar', () => setEditable(value), showSnackbar)}
+						setEditable={value =>
+							props.authHandler(
+								'modificar',
+								() => setEditable(value),
+								showSnackbar
+							)
+						}
 						editable={editable}
 						handleChange={handleChange}
 						loading={salud.loading}
-						sendData={() => props.authHandler('modificar', sendData, showSnackbar)}
+						sendData={() =>
+							props.authHandler('modificar', sendData, showSnackbar)
+						}
 						data={saludItem}
 						fields={salud.errorFields}
 						messages={salud.errorMessages}
