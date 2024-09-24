c5553030bb792fe0d1fc502ad4eaf6ec76ba765a
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-09-24 09:30:57 -0600

    Bugfix

--- src/views/app/director/ExpedienteEstudiante/_partials/contacto/HistoricoResidencia.tsx
+++ src/views/app/director/ExpedienteEstudiante/_partials/contacto/HistoricoResidencia.tsx
@@ -3,12 +3,7 @@ import React, { useEffect, useMemo, useState } from 'react'
 import { useActions } from 'Hooks/useActions'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
-import {
-	Input,
-	Label,
-	Row,
-	Col, FormGroup, Form
-} from 'reactstrap'
+import { Input, Label, Row, Col, FormGroup, Form } from 'reactstrap'
 import { WebMapView } from './MapView'
 import OptionModal from 'Components/Modal/OptionModal'
 import { Card, CardBody, Modal, ModalBody } from 'reactstrap'
@@ -21,11 +16,10 @@ import { useSelector } from 'react-redux'
 
 import moment from 'moment'
 import { useTranslation } from 'react-i18next'
-const useStyles = makeStyles((theme) => ({
-
+const useStyles = makeStyles(theme => ({
 	control: {
 		padding: theme.spacing(2)
-	},
+	}
 }))
 const HistoricoResidencia = ({ identidadId }) => {
 	const { t } = useTranslation()
@@ -42,23 +36,38 @@ const HistoricoResidencia = ({ identidadId }) => {
 			{
 				label: 'Fecha y hora',
 				column: 'fechaInsercion',
-				Header: t('estudiantes>expediente>contacto>historico_residencia>fecha_hora', 'Fecha y hora'),
+				Header: t(
+					'estudiantes>expediente>contacto>historico_residencia>fecha_hora',
+					'Fecha y hora'
+				),
 				accessor: 'fechaInsercion',
 				Cell: ({ row }) => {
-					return <div>{moment(row.original?.fechaInsercion)?.format('DD/MM/YYYY HH:mm')} </div>
+					return (
+						<div>
+							{moment(row.original?.fechaInsercion)?.format('DD/MM/YYYY HH:mm')}{' '}
+						</div>
+					)
 				}
 			},
 			{
-				Header: t('estudiantes>expediente>contacto>historico_residencia>tipo', 'Tipo'),
+				Header: t(
+					'estudiantes>expediente>contacto>historico_residencia>tipo',
+					'Tipo'
+				),
 				accessor: 'tipo',
 				label: 'Tipo',
 				column: 'tipo',
 				Cell: ({ row }) => {
-					return <div>{row.original?.tipo === 0 ? 'Residencia' : 'Temporal'}</div>
+					return (
+						<div>{row.original?.tipo === 0 ? 'Residencia' : 'Temporal'}</div>
+					)
 				}
 			},
 			{
-				Header: t('estudiantes>expediente>contacto>historico_residencia>usuario', 'Usuario'),
+				Header: t(
+					'estudiantes>expediente>contacto>historico_residencia>usuario',
+					'Usuario'
+				),
 				accessor: 'nombreUsuario',
 				label: 'Usuario',
 				column: 'user'
@@ -70,7 +79,7 @@ const HistoricoResidencia = ({ identidadId }) => {
 				column: 'actions',
 				Cell: ({ row }) => {
 					return (
-						<div className='d-flex justify-content-center align-items-center'>
+						<div className="d-flex justify-content-center align-items-center">
 							<Tooltip title={t('general>ver', 'Ver')}>
 								<VisibilityIcon
 									style={{
@@ -103,11 +112,22 @@ const HistoricoResidencia = ({ identidadId }) => {
 		}))
 	}, [bitacoraResidencia])
 	useEffect(() => {
-		if (selectedRow?.json?.latitude && selectedRow?.json?.longitude && selectedRow?.json?.longitude != "" && selectedRow?.json?.latitude != ""  && search) {
+		if (
+			selectedRow?.json?.latitude &&
+			selectedRow?.json?.longitude &&
+			selectedRow?.json?.longitude != '' &&
+			selectedRow?.json?.latitude != '' &&
+			search
+		) {
 			search?.search([selectedRow.json?.longitude, selectedRow.json?.latitude])
 			setShowCoordinates(true)
-		} else if (selectedRow?.json?.province?.label && selectedRow?.json?.canton?.label && selectedRow?.json?.distrito.label && search) {
-			search?.searchTerm = `${selectedRow?.json?.province?.label}, ${selectedRow?.json?.canton?.label}, ${selectedRow?.json?.distrito.label}`
+		} else if (
+			selectedRow?.json?.province?.label &&
+			selectedRow?.json?.canton?.label &&
+			selectedRow?.json?.distrito.label &&
+			search
+		) {
+			search.searchTerm = `${selectedRow?.json?.province?.label}, ${selectedRow?.json?.canton?.label}, ${selectedRow?.json?.distrito.label}`
 			search?.search(`${search.searchTerm}, CRI`)
 			setShowCoordinates(false)
 		}
@@ -117,129 +137,135 @@ const HistoricoResidencia = ({ identidadId }) => {
 			<Card>
 				<CardBody>
 					<OptionModal
-						size='xl'
+						size="xl"
 						hideCancel
 						isOpen={selectedRow && true}
 						titleHeader={'Historico de residencia'}
-						onConfirm={() =>
-							setSelectedRow(false)}
+						onConfirm={() => setSelectedRow(false)}
 					>
 						<Row>
 							<Col xs={4}>
-
 								<FormGroup>
-									<Label for='province'>Provincia</Label>
+									<Label for="province">Provincia</Label>
 									<Input
-										type='text'
-										name='province'
+										type="text"
+										name="province"
 										value={selectedRow?.json?.province.label}
-										id='province'
+										id="province"
 										disabled
 									/>
 								</FormGroup>
 
 								<FormGroup>
-									<Label for='distrito'>Distrito</Label>
+									<Label for="canton">Cantón</Label>
 									<Input
-										type='text'
-										name='distrito'
-										value={selectedRow?.json?.distrito.label}
-										id='distrito'
-										disabled
-									/>
-								</FormGroup>
-
-								<FormGroup>
-									<Label for='poblado'>Poblado</Label>
-									<Input
-										type='text'
-										name='poblado'
-										value={selectedRow?.json?.poblado.label}
-										id='poblado'
-										disabled
-									/>
-								</FormGroup>
-
-
-								<FormGroup>
-									<Label for='canton'>Cantón</Label>
-									<Input
-										type='text'
-										name='canton'
+										type="text"
+										name="canton"
 										value={selectedRow?.json?.canton.label}
-										id='canton'
+										id="canton"
 										disabled
 									/>
 								</FormGroup>
 
-							</Col>
-							<Col xs={8}
-								style={{
+								<FormGroup>
+									<Label for="distrito">Distrito</Label>
+									<Input
+										type="text"
+										name="distrito"
+										value={selectedRow?.json?.distrito.label}
+										id="distrito"
+										disabled
+									/>
+								</FormGroup>
 
+								<FormGroup>
+									<Label for="poblado">Poblado</Label>
+									<Input
+										type="text"
+										name="poblado"
+										value={selectedRow?.json?.poblado.label}
+										id="poblado"
+										disabled
+									/>
+								</FormGroup>
+							</Col>
+							<Col
+								xs={8}
+								style={{
 									textAlign: 'left',
 									justifyContent: 'left',
 									alignItems: 'left'
-								}}>
-
-								<WebMapView
-									setSearch={setSearch}
-								/>
+								}}
+							>
+								<WebMapView setSearch={setSearch} />
 							</Col>
 							<Col xs={12}>
-
 								<FormGroup>
-									<Label for='direccion'>Dirección</Label>
+									<Label for="direccion">Dirección</Label>
 									<Input
-										type='textarea'
-										name='direccion'
+										type="textarea"
+										name="direccion"
 										value={selectedRow?.json?.direccionExacta}
-										id='direccion'
+										id="direccion"
 										disabled
 									/>
 								</FormGroup>
 							</Col>
-							{showCoordinates && (<><Col xs={6} style={{
+							{showCoordinates && (
+								<>
+									<Col
+										xs={6}
+										style={{
+											textAlign: 'left',
+											justifyContent: 'left',
+											alignItems: 'left'
+										}}
+									>
+										<FormGroup>
+											<Label for="latitude">Latitud</Label>
+											<Input
+												type="text"
+												name="latitude"
+												value={selectedRow?.json?.latitude}
+												id="latitude"
+												disabled
+											/>
+										</FormGroup>
+									</Col>
 
-								textAlign: 'left',
-								justifyContent: 'left',
-								alignItems: 'left'
-							}}><FormGroup>
-									<Label for='latitude'>Latitud</Label>
-									<Input
-										type='text'
-										name='latitude'
-										value={selectedRow?.json?.latitude}
-										id='latitude'
-										disabled
-									/>
-								</FormGroup></Col>
-
-								<Col xs={6} style={{
-
-									textAlign: 'left',
-									justifyContent: 'left',
-									alignItems: 'left'
-								}}><FormGroup>
-										<Label for='longitude'>Longitud</Label>
-										<Input
-											type='text'
-											name='longitude'
-											value={selectedRow?.json?.longitude}
-											id='longitude'
-											disabled
-										/>
-									</FormGroup></Col></>)}
-
+									<Col
+										xs={6}
+										style={{
+											textAlign: 'left',
+											justifyContent: 'left',
+											alignItems: 'left'
+										}}
+									>
+										<FormGroup>
+											<Label for="longitude">Longitud</Label>
+											<Input
+												type="text"
+												name="longitude"
+												value={selectedRow?.json?.longitude}
+												id="longitude"
+												disabled
+											/>
+										</FormGroup>
+									</Col>
+								</>
+							)}
 						</Row>
 					</OptionModal>
 
-
 					<h1>
-						{t('estudiantes>expediente>contacto>historico_residencia>titulo', 'Histórico de residencias')}
+						{t(
+							'estudiantes>expediente>contacto>historico_residencia>titulo',
+							'Histórico de residencias'
+						)}
 					</h1>
 					<TableReactImplementation columns={columns} data={data} />
 				</CardBody>
-			</Card >
+			</Card>
 		</>
 	)
 }
