4f0050c2f92f1d5344d5a37aaa3a89711b06df44
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-09-26 08:46:48 -0600

    Minor fix validation ids

--- src/views/app/director/ExpedienteEstudiante/Contacto.js
+++ src/views/app/director/ExpedienteEstudiante/Contacto.js
@@ -32,7 +32,7 @@ const InformacionResidenciaFija = withAuthorization({
 })(InformacionResidenciaSaber)
 
 const HistoricoResidenciaAuth = withAuthorization({
-	id: 104,
+	id: 102,
 	Modulo: 'Expediente Estudiantil',
 	Apartado: 'Informacion de Contacto',
 	Seccion: 'Historico de residencias'
