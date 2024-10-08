b64daba650d585581663ac0df8c82eb34742e9a1
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-10-08 10:40:00 -0600

    Cambios: Datos registrales

--- src/views/app/director/ExpedienteEstudiante/General.js
+++ src/views/app/director/ExpedienteEstudiante/General.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import useNotification from 'Hooks/useNotification'
 import HeaderTab from 'Components/Tab/Header'
 import ContentTab from 'Components/Tab/Content'
@@ -8,16 +8,12 @@ import InfoGeneral from './_partials/general/InfoGeneral'
 import DatosRegistrales from './_partials/general/DatosRegistrales'
 import { useSelector } from 'react-redux'
 
-const optionsTab = [
-	{ title: 'Información personal' },
-	{ title: 'Datos registrales' }
-]
+let optionsTab = [{ title: 'Información personal' }]
 
-const General = (props) => {
+const General = props => {
 	const { t } = useTranslation()
 	const [activeTab, setActiveTab] = useState(0)
 	const [snackBar, handleClick] = useNotification()
-
 
 	const state = useSelector(store => {
 		return {
@@ -42,16 +38,22 @@ const General = (props) => {
 		handleClick()
 	}
 
+	useEffect(() => {
+		if (
+			state.expedienteEstudiantil.currentStudent.tipoIdentificacion === 'CÉDULA'
+		) {
+			optionsTab.push({ title: 'Datos registrales' })
+		}
+	}, [])
+
 	return (
 		<div>
-			<br />
 			<h4>
 				{t(
 					'estudiantes>expediente>info_gen>info_gen>titulo',
 					'Información general'
 				)}
 			</h4>
-			<br />
 			{snackBar(snackbarContent.variant, snackbarContent.msg)}
 			<HeaderTab
 				options={optionsTab}
@@ -65,8 +67,17 @@ const General = (props) => {
 						toggleSnackbar={toggleSnackbar}
 						handleClick={handleClick}
 					/>
-				)} 
-				{activeTab === 1 && state.expedienteEstudiantil.currentStudent.tipoIdentificacion != "CÉDULA" && < DatosRegistrales />}
+				)}
+				{activeTab === 1 &&
+					state.expedienteEstudiantil.currentStudent.tipoIdentificacion ===
+						'CÉDULA' && (
+						<DatosRegistrales
+							toggleSnackbar={toggleSnackbar}
+							identificacion={
+								state.expedienteEstudiantil.currentStudent.identificacion
+							}
+						/>
+					)}
 			</ContentTab>
 		</div>
 	)
--- src/views/app/director/ExpedienteEstudiante/_partials/general/DatosRegistrales.js
+++ src/views/app/director/ExpedienteEstudiante/_partials/general/DatosRegistrales.js
@@ -6,6 +6,8 @@ import withAuthorization from 'Hoc/withAuthorization'
 import { withIdentification } from 'Hoc/Identification'
 import BarLoader from 'Components/barLoader/barLoader'
 import axios from 'axios'
+import { isEmpty } from 'lodash'
+import colors from 'assets/js/colors'
 import { useTranslation } from 'react-i18next'
 
 const DatosRegistrales = props => {
@@ -14,22 +16,53 @@ const DatosRegistrales = props => {
 	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
-		axios
-			.get(
-				`${envVariables.BACKEND_URL}/api/TSEIdentidad/GetOneByCedula/${props.identification.data.identificacion}`
-			)
-			.then(r => {
-				setEstudiante(r.data)
-				setLoading(false)
-			})
+		if (props.identificacion) {
+			axios
+				.get(
+					`${envVariables.BACKEND_URL}/api/TSEIdentidad/GetOneByCedula/${props.identificacion}`
+				)
+				.catch(e => {
+					props.toggleSnackbar(
+						'error',
+						'Error: Problema de conexión con el TSE'
+					)
+				})
+				.then(r => {
+					if (!isEmpty(r?.data)) {
+						setEstudiante(r.data)
+					}
+				})
+				.finally(() => {
+					setLoading(false)
+				})
+		}
 	}, [props.identificacion])
 
 	if (loading) {
 		return <BarLoader />
 	}
 
+	if (isEmpty(estudiante)) {
+		return (
+			<>
+				<h1 style={{ color: colors.error }}>
+					<i class="fas fa-exclamation-triangle"></i>
+					{' Problema de conexión con el TSE'}
+				</h1>
+			</>
+		)
+	}
+
 	return (
 		<Row className="mb-3">
+			<Col xs={12}>
+				<p className="mb-3">
+					<i class="fas fa-info-circle"></i> En esta pantalla muestra la
+					información de los progenitores de la persona estudiante, suministrada
+					por el Tribunal Supremo de Elecciones (T.S.E):
+				</p>
+			</Col>
+
 			<Col lg={6}>
 				<FormProgenitor
 					nombre={estudiante.nombreMadrePadreRegistral1}
